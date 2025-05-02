e29af3de51eb52fadf4fe1042352e173e6fc588a
CallumCPP <callumtomkins260505@gmail.com>
2025-05-03 00:33:24 +0100

    Improve cleanup with proxy_manager

--- admin/app.py
+++ admin/app.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
@@ -9,6 +10,7 @@ app = FastAPI()
 # Serve pages directory as a static site
 app.mount("/pages", StaticFiles(directory="admin/pages"), "pages")
 
+
 # Endpoint to spawn instance of proxy
 @app.post("/proxy/new", tags=["Proxy management"])
 async def new_proxy() -> int:
@@ -19,3 +21,12 @@ async def new_proxy() -> int:
 @app.delete("/proxy/{proxy_id}", tags=["Proxy management"])
 async def terminate_proxy(proxy_id: int):
     await proxy_manager.terminate(proxy_id)
+
+
+# Clean up proxies on shutdown
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Before shutdown
+    yield
+    # During shutdown
+    proxy_manager.cleanup()
--- proxy_manager.py
+++ proxy_manager.py
@@ -1,6 +1,4 @@
-import asyncio
-from multiprocessing import Process, Lock, process
-from typing import Any, Callable
+from multiprocessing import Process, Lock
 
 from fastapi import HTTPException
 
@@ -10,6 +8,7 @@ from proxy.tcp_server import TCPServer
 
 def _run_proxy():
     # Initialise TCPServer
+    server: TCPServer | None
     try:
         server = TCPServer((config.host, config.port), RequestHandler)
         print(f"Running proxy on {config.host}:{config.port}",
@@ -19,25 +18,24 @@ def _run_proxy():
         server.serve_forever()
     except OSError as e:
         print(f"OS Error starting server: {str(e)}")
+        exit(e.errno)
 
 
 class _ProxyManager:
     proxies_lock = Lock()
     proxies: list[Process] = []
 
-    def __del__(self):
+    def cleanup(self):
         # Terminate proxy servers when this object is deleted
         for process in self.proxies:
-            if not process:
-                continue
-
             process.terminate()
             process.join()
+            process.close()
 
     def get_new_id(self) -> int | None:
         # Either find the first empty spot in the list or return the max
         for i in range(len(self.proxies)):
-            if not self.proxies[i].is_alive():
+            if self.proxies[i].exitcode is not None:
                 return i
 
         return None
@@ -48,19 +46,20 @@ class _ProxyManager:
             proxy_id = self.get_new_id()
             process = Process(target=_run_proxy)
 
-            if not proxy_id:
+            if proxy_id is None:
                 self.proxies.append(process)
                 proxy_id = len(self.proxies) - 1
             else:
+                self.proxies[proxy_id].join()
+                self.proxies[proxy_id].close()
                 self.proxies[proxy_id] = process
         
         process.start()
-        print(self.proxies)
         return proxy_id
 
     async def terminate(self, proxy_id: int):
         # If the process_id doesn't point to a process, raise HTTP 404
-        if proxy_id > len(self.proxies) or self.proxies[proxy_id] is None:
+        if proxy_id > len(self.proxies)-1 or self.proxies[proxy_id].exitcode is not None:
             raise HTTPException(status_code=404, detail=f"Process with id {proxy_id} was not found.")
         
         # Get the proxy and assert its existence as it should exist
