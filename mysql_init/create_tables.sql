881e47fdc31b5787a60546aafe0646198fb9fa23
KapaBaws <kasperkula@gmail.com>
2025-05-05 18:10:53 +0100

    created a table to store whitelist/ blacklist

--- mysql_init/create_tables.sql
+++ mysql_init/create_tables.sql
@@ -1,3 +1,4 @@
+-- A table to store configurations data for the proxy
 CREATE TABLE IF NOT EXISTS configs (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(64) UNIQUE NOT NULL,
@@ -13,8 +14,18 @@ CREATE TABLE IF NOT EXISTS configs (
     max_requests_per_second FLOAT
 );
 
+-- A table to store the users
 CREATE TABLE IF NOT EXISTS users (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(64) NOT NULL,
     password VARCHAR(64) NOT NULL
+);
+
+-- A table to store whitelist and blacklist data
+CREATE TABLE IF NOT EXISTS access_control (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    ip_address VARCHAR(64) NOT NULL,
+    list_type ENUM('whitelist', 'blacklist') NOT NULL,
+    config_id INT UNSIGNED NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
